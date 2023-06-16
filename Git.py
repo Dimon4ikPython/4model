ae767e1c9832fdd59e1430d9c38ab4e23e5c35e0
Дмитрий <dimasmelkov29@gmail.com>
2023-06-16 12:27:10 +0300

    fisrt commit

---
+++ Git.py
@@ -0,0 +1,29 @@
+# def strcounter(string): # Сложность O(n ** 2)
+#     for symbol in string:
+#         counter = 0
+#         for sub_stmbol in string:
+#             if symbol == sub_stmbol:
+#                 counter+=1
+#         print(symbol, counter)
+#
+# strcounter('abcdcdab')
+
+
+# def strcounter(string): # Сложность O(n * m)
+#     for symbol in set(string):
+#         counter = 0
+#         for sub_stmbol in string:
+#             if symbol == sub_stmbol:
+#                 counter+=1
+#         print(symbol, counter)
+#
+# strcounter('abcdcdab')
+
+
+def strcounter(string):
+    syms_counter = {}
+    for symbol in string:
+        syms_counter[symbol] = syms_counter.get(symbol, 0) + 1
+    print(syms_counter)
+
+strcounter('aaabbccdee')
