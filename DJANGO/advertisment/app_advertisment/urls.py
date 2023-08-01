bcb7fd7da2b47374ae83d11d9571aced4959ce85
Дмитрий <dimasmelkov29@gmail.com>
2023-08-01 13:33:29 +0300

    commit 7

--- DJANGO/advertisment/app_advertisment/urls.py
+++ DJANGO/advertisment/app_advertisment/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, top_seller
 
 
 urlpatterns = [
-    path("", index),
-    path("start", index)
+    path("", index, name='main_page'),
+    path("top-sellers/", top_seller, name='top_sellers')
 ]
--- DJANGO/advertisment/app_lesson_4/urls.py
+++ DJANGO/advertisment/app_lesson_4/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from .views import lesson
 urlpatterns = [
-    path('lesson_4', lesson)
+    path('', lesson) #"lesson_4"
 ]
