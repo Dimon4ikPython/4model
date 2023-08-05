e4f499cd50f0ce683eb6c0fa34a9194a06f864df
Дмитрий <dimasmelkov29@gmail.com>
2023-08-05 16:58:47 +0300

    commit 8

--- DJANGO/advertisment/app_advertisment/admin.py
+++ DJANGO/advertisment/app_advertisment/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Advertisement
 
-# Register your models here.
+
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'price', 'created_date', 'auction']
+    list_filter = ['auction', 'created_at']
+    actions = ['make_auction_as_false', 'make_auction_as_true']
+    #search_fields = []
+
+@admin.action(description='Убрать возможность торга')
+def make_auction_as_false(self, reqquest, queryset):
+    queryset.update(auction=False)
+
+@admin.action(description='Добвавить возможность торга')
+def make_auction_as_false(self, reqquest, queryset):
+    queryset.update(auction=True)
+
+
+admin.site.register(Advertisement, AdvertisementAdmin)
